aff1b5170c18c3f2e56849be8f6a2204c48e005e
Patrick Stinnett <pstinnett@gmail.com>
2010-02-14 17:20:22 -0500

    first commit with authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.resources :users
+
+  map.resource :session
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -45,4 +53,8 @@ ActionController::Routing::Routes.draw do |map|
   map.scrape '/scrape', :controller => 'artists', :action => 'load'
   map.scrape '/about', :controller => 'pages', :action => 'about'
   map.import_tracks '/import/:id', :controller => 'tracks', :action => 'import'
+  map.signup  '/signup', :controller => 'users',   :action => 'new'
+  map.login  '/login',  :controller => 'session', :action => 'new'
+  map.logout '/logout', :controller => 'session', :action => 'destroy'
+  map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
 end
